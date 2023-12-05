4aae27bd2ec45590bb6fff0badf533bdffb7c669
morrison <morrisonwanja@gmail.com>
2023-12-05 17:14:43 +0300

    Complete Lab

---
+++ lib/create.sql
@@ -0,0 +1,1 @@
+CREATE TABLE bears(id INTEGER PRIMARY KEY,name TEXT,age INTEGER, sex Text, color TEXT,temperament TEXT,alive BOOLEAN)
---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears(name,age,sex,color,temperament,alive) 
+VALUES ("Mr. Chocolate",4,"M","Black","low",1),
+       ("Rowdy",13,"M","Black","low",1),
+       ("Tabitha",6,"F","Black","low",1),
+       ("Sergeant Brown",3,"M","Black","low",1),
+       ("Melissa",13,"F","Black","low",1),
+       ("Grinch",9,"M","Black","low",1),
+       ("Wendy",6,"F","Black","low",1),
+       (NULL,11,"M","Black","low",1)
